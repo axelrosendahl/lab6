be1a2ebe9edc04325b3883d52fa90513c2b22d81
axero335 <axero335@roxen2-112.ad.liu.se>
2019-12-10 10:18:27 +0100

    First commit

---
+++ MenuItem.java
@@ -0,0 +1,13 @@
+package lab6;
+
+public interface MenuItem {
+
+	/**
+	* Returnerar menyvalets rubrik.
+	*/
+	public String getTitle();
+	/**
+	* Exekverar/v�ljer menyvalet.
+	*/
+	public void execute();
+}
